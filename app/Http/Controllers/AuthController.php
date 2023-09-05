0fcda04b51359a1773a6e843484ac0f2358921ac
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2023-09-05 16:59:32 +0100

    register function loaded

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Auth;
+use Psy\Readline\Userland;
 use Validator;
 
 class AuthController extends Controller
@@ -12,12 +14,29 @@ class AuthController extends Controller
     {
         $this->middleware(
             'auth:api',
-            [ 'except' => ['login', 'register'] ]
+            ['except' => ['login', 'register']]
         );
     }
 
     public function register(Request $request)
     {
-        return 'welcome';
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|string|email|unique:users',
+            'passsword' => 'required|string|confirmed|min:6'
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+        $user = User::create(
+            array_merge(
+                $validator->validated(),
+                ['password' => bcrypt($request->password)]
+            )
+        );
+        return response()->json([
+            'message' => 'User Successfully Registered',
+            'User' => $user
+        ], 201);
     }
 }
